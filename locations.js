b910a079c5e936de9a001a7c18286817723e8de4
adityarao310 <hello@adityarao.name>
2017-10-10 18:17:20 +0530

    Beautified map
    - markers have labels
    - markers are connected with driving directions
    - diff icon for airport and normal location
    - put locations.js and styles.js separately
    - geodesic lines between airports

---
+++ locations.js
@@ -0,0 +1,79 @@
+// All the locations. Get from DB database later.
+
+// main location of the office, center of map
+var office = {
+    lat: 12.9354823,
+    lng: 77.6302228
+};
+
+var locations = [{
+        title: 'Office',
+        airport: false,
+        location: office,
+        number: 1,
+    },
+
+    {
+        title: 'Third Wave Coffee Roasters',
+        airport: false,
+        number: 2,
+        location: {
+            lat: 12.9348112,
+            lng: 77.6274063,
+        }
+    },
+    {
+        title: 'Bangalore Airport',
+        airport: true,
+        number: 3,
+        location: {
+            lat: 13.19864,
+            lng: 77.7044041,
+        }
+    },
+    {
+        title: 'Thate Idli Bidadi',
+        airport: false,
+        number: 4,
+        location: {
+            lat: 12.7849804,
+            lng: 77.3751219,
+        }
+    },
+    {
+        title: 'Manchanabele Camping',
+        airport: false,
+        number: 5,
+        location: {
+            lat: 12.8697503,
+            lng: 77.3355362,
+        }
+    },
+    {
+        title: 'Mylari Benne Dosa',
+        airport: false,
+        number: 6,
+        location: {
+            lat: 12.3084519,
+            lng: 76.6632816,
+        }
+    },
+    {
+        title: 'Ecoscape Wildlife',
+        airport: false,
+        number: 7,
+        location: {
+            lat: 11.4626301,
+            lng: 76.3597993,
+        }
+    },
+    {
+        title: 'Bishkek Airport',
+        airport: true,
+        number: 8,
+        location: {
+            lat: 12.9941172,
+            lng: 80.1686781,
+        }
+    },
+];
--- map.js
+++ map.js
@@ -1,269 +1,87 @@
-var map;
-// custom marker icon in the map.
-var markerImage = 'images/marker.png';
-
-// empty markers list to be used later
+// custom marker icon in the map var markerImage = 'images/marker.png';
+// global arrays and variables to be used later
 var markers = [];
+var airports = [];
+var flightLine, map, mainInfowindow;
+var directionsService, directionsDisplay;
 
-// Create a styles array to use with the map.
-// taken from http://www.mapstylr.com/style/bentley/
-var styles = [{
-        elementType: 'geometry',
-        stylers: [{
-            color: '#ebe3cd'
-        }]
-    },
-    {
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#523735'
-        }]
-    },
-    {
-        elementType: 'labels.text.stroke',
-        stylers: [{
-            color: '#f5f1e6'
-        }]
-    },
-    {
-        featureType: 'administrative',
-        elementType: 'geometry.stroke',
-        stylers: [{
-            color: '#c9b2a6'
-        }]
-    },
-    {
-        featureType: 'administrative.land_parcel',
-        elementType: 'geometry.stroke',
-        stylers: [{
-            color: '#dcd2be'
-        }]
-    },
-    {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#ae9e90'
-        }]
-    },
-    {
-        featureType: 'landscape.natural',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#dfd2ae'
-        }]
-    },
-    {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#dfd2ae'
-        }]
-    },
-    {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#93817c'
-        }]
-    },
-    {
-        featureType: 'poi.park',
-        elementType: 'geometry.fill',
-        stylers: [{
-            color: '#a5b076'
-        }]
-    },
-    {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#447530'
-        }]
-    },
-    {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#f5f1e6'
-        }]
-    },
-    {
-        featureType: 'road.arterial',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#fdfcf8'
-        }]
-    },
-    {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#f8c967'
-        }]
-    },
-    {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{
-            color: '#e9bc62'
-        }]
-    },
-    {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#e98d58'
-        }]
-    },
-    {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry.stroke',
-        stylers: [{
-            color: '#db8555'
-        }]
-    },
-    {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#806b63'
-        }]
-    },
-    {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#dfd2ae'
-        }]
-    },
-    {
-        featureType: 'transit.line',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#8f7d77'
-        }]
-    },
-    {
-        featureType: 'transit.line',
-        elementType: 'labels.text.stroke',
-        stylers: [{
-            color: '#ebe3cd'
-        }]
-    },
-    {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [{
-            color: '#dfd2ae'
-        }]
-    },
-    {
-        featureType: 'water',
-        elementType: 'geometry.fill',
-        stylers: [{
-            color: '#b9d3c2'
-        }]
-    },
-    {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#92998d'
-        }]
-    }
-];
-
-// main map function
+// main map function loaded
 function initMap() {
-
-    // main location of the office, center of map
-    var office = {
-        lat: 12.9354823,
-        lng: 77.6302228
-    };
-
     // create the map
     map = new google.maps.Map(document.getElementById('map'), {
         center: office,
         zoom: 7,
-        styles: styles,
+        styles: styles, // coming from styles.js
         mapTypeControl: false,
     });
 
-    // All the locations. Get from DB database later.
-    var locations = [{
-            title: 'Office',
-            location: office
-        },
-
-        {
-            title: 'Third Wave Coffee Roasters',
-            location: {
-                lat: 12.9348112,
-                lng: 77.6274063
-            }
-        },
-        {
-            title: 'Thate Idli Bidadi',
-            location: {
-                lat: 12.7849804,
-                lng: 77.3751219
-            }
-        },
-        {
-            title: 'Manchanabele Camping',
-            location: {
-                lat: 12.8697503,
-                lng: 77.3355362
-            }
-        },
-        {
-            title: 'Mylari Benne Dosa',
-            location: {
-                lat: 12.3084519,
-                lng: 76.6632816
-            }
-        },
-        {
-            title: 'Ecoscape Wildlife',
-            location: {
-                lat: 11.4626301,
-                lng: 76.3597993
-            }
-        },
-    ];
-
-    var mainInfowindow = new google.maps.InfoWindow({
+    // initialise InfoWindow
+    mainInfowindow = new google.maps.InfoWindow({
         maxWidth: 400,
     });
 
-    // Create an array of markers on starting up by looping
+    // Store markers and airpots in map from locations.js
     for (var i = 0; i < locations.length; i++) {
-        // get position and title from DB
+        // create location markers, not airport
+        // get position and title from DB later
         var position = locations[i].location;
         var title = locations[i].title;
-        // create a marker for every location
-        var marker = new google.maps.Marker({
-            position: position,
-            // map: map, element to be inserted on button click, not needed here
-            title: title,
-            animation: google.maps.Animation.DROP,
-            icon: markerImage,
-            id: i
-        });
-        // push each marker into the empty array for use later
-        markers.push(marker);
-        // open main info window on click
-        marker.addListener('click', function() {
-            populateInfoWindow(this, mainInfowindow);
-        });
+        // create diff icons for airprot and location
+        if (locations[i].airport === false) {
+            // create a marker for every location
+            var marker = new google.maps.Marker({
+                position: position,
+                // map: map, // element to be inserted on click, not needed here
+                title: title,
+                animation: google.maps.Animation.DROP,
+                id: i,
+                label: locations[i].number.toString(),
+            });
+            // push each marker into the empty array for use later
+            markers.push(marker);
+            // open main info window on click
+            marker.addListener('click', function() {
+                populateInfoWindow(this, mainInfowindow);
+            });
+        } else if (locations[i].airport === true) {
+            var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+            var airport = new google.maps.Marker({
+                position: position,
+                // map: map, // element to be inserted on click, not needed here
+                title: title,
+                animation: google.maps.Animation.DROP,
+                id: i,
+                label: locations[i].number.toString(),
+                icon: image,
+            });
+            // push each airport into the empty array for use later
+            airports.push(airport);
+            // open main info window on click
+            airport.addListener('click', function() {
+                populateInfoWindow(this, mainInfowindow);
+            });
+        };
     };
+
+    // flight line design and create new geodesic polyline
+    flightLine = new google.maps.Polyline({
+        strokeColor: '#CC0099',
+        strokeOpacity: 1.0,
+        strokeWeight: 3,
+        // map: map,
+        geodesic: true,
+    });
+
+    // path definition @ origin and destination
+    var path = [airports[0].getPosition(), airports[1].getPosition()];
+    flightLine.setPath(path);
 
     // event listeners when the hide, show buttons are clicked
     document.getElementById('show-listings').addEventListener('click', showListings);
     document.getElementById('hide-listings').addEventListener('click', hideListings);
-}
+    document.getElementById('show-routes').addEventListener('click', showRoutes);
+    document.getElementById('hide-routes').addEventListener('click', hideRoutes);
+};
 
 // function to populate info window of marker
 function populateInfoWindow(marker, infowindow) {
@@ -304,13 +122,17 @@ function populateInfoWindow(marker, infowindow) {
 // This function will loop through the markers array and display them all.
 function showListings() {
     var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    // northeast <> southwest of map
+    // Extend the boundaries NE <> SW of map and display markers
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(map);
         bounds.extend(markers[i].position);
     }
+    for (var i = 0; i < airports.length; i++) {
+        airports[i].setMap(map);
+        bounds.extend(airports[i].position);
+    }
     map.fitBounds(bounds);
+    flightLine.setMap(map); // show flightline
 }
 
 // This function will loop through the listings and hide them all.
@@ -318,4 +140,34 @@ function hideListings() {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
+    for (var i = 0; i < airports.length; i++) {
+        airports[i].setMap(null);
+    }
+    flightLine.setMap(null);
 }
+
+// display the routes and paths
+function showRoutes(directionsService, directionsDisplay) {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
+    var routeBetween = {
+        origin: markers[2].position,
+        destination: markers[3].position,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+    }
+
+    directionsService.route(routeBetween, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            directionsDisplay.setMap(map);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+};
+
+// display the routes and paths
+function hideRoutes(directionsService, directionsDisplay) {
+    directionsDisplay.setMap(null);
+};
